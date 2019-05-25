2b204913b5d99945cf259a739f3c089fc2a697fb
Nate Wang <supnate@gmail.com>
2019-05-26 02:10:06 +0800

    Add rekit json config.

---
+++ postCreate.js
@@ -0,0 +1,24 @@
+'use strict';
+
+const path = require('path');
+const fs = require('fs-extra');
+const _ = require('lodash');
+
+const prjPath = __dirname;
+
+function postCreate(args) {
+    // Remove unnecessary files
+    ['.travis.yml', 'yarn.lock', 'LICENSE', 'package-lock.json']
+        .map(f => path.join(prjPath, f))
+        .forEach(file => fs.existsSync(file) && fs.unlinkSync(file));
+    const name = _.kebabCase(args.name);
+
+    // Clean package.json
+    const pkgJsonPath = path.join(prjPath, 'package.json');
+    const pkgJson = require(pkgJsonPath); // eslint-disable-line
+
+    pkgJson.name = name;
+    fs.writeFileSync(pkgJsonPath, JSON.stringify(pkgJson, null, '  '));
+}
+
+module.exports = postCreate;
